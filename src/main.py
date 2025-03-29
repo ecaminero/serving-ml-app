4b69d78d2e8ca9f34e08fbde2b65341ee052344c
Edwin <ecaminero@>
2025-03-29 00:04:44 -0300

    build(health): adding health config

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+"""Entrypoint to invoke the FastAPI application service with."""
+
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+    status: str = "OK"
 
-@app.get("/")
-async def root():
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    return HealthCheck(status="OK")
+
+
+@app.post("/predict", tags=["predict"])
+async def predict():
     return {"message": "Hello World"}
